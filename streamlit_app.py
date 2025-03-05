afb51f8cad2581a06ff41d361b9a32a74198aaf1
mattlombardo18 <146169663+mattlombardo18@users.noreply.github.com>
2025-03-05 10:50:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,36 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-# Import your Black-Scholes and Greeks functions
-from black_scholes_module import black_scholes, calculate_greeks
+import numpy as np 
+from scipy.stats import norm
+
+
+
+def black_scholes(S,K,T,r,sigma,option_type="call"):
+  d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+  d2 = d1 - sigma * np.sqrt(T)
+
+  if option_type == "call":
+        price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+  elif option_type == "put":
+        price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+  else:
+        raise ValueError("Invalid option type. Use 'call' or 'put'.")
+    
+  return price
+ 
+    
+# Greeks
+
+def calculate_greeks(S, K, T, r, sigma):
+    d1 = (np.log(S/K)+(r+0.5 * sigma**2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    delta = norm.cdf(d1) # Delta
+    gamma = norm.pdf(d1) / (S * sigma * np.sqrt(T) ) # Gamma
+    vega = S * norm.pdf(d1) * np.sqrt(T) # Vega
+    theta = - (S * norm.pdf(d1) * sigma) / (2 * np.sqrt(T)) - r * K * np.exp(-r * T) * norm.cdf(d2)# Theta
+    rho = K * T * np.exp(-r * T) * norm.cdf(d2) # Rho 
+    return delta, gamma, vega, theta, rho
 
 # Streamlit App
 st.title("Options Pricing Dashboard")
